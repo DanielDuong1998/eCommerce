3ccb5e6b6d7e980f166856c4e9adb09f4c34bcbf
khang.duong/PC <dvkhangnt@gmail.com>
2024-05-22 08:24:04 +0700

    logout

--- src/services/access.service.js
+++ src/services/access.service.js
@@ -18,6 +18,13 @@ const RoleShop = {
 }
 
 class AccessService {
+
+    static logout = async(keyStore) =>{
+        const delKey = await KeyTokenService.removeKeyById(keyStore._id);
+        console.log('delKey: ', delKey);
+        return delKey;
+    }
+
     /**
      * 1 - Check email in dbs
      * 2 - match password
